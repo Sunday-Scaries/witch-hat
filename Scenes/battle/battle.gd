8a729819c86fe55de72a207864982b34518b5ae0
brianedelman <bd.edelman@gmail.com>
2024-11-29 13:49:43 -0500

    make modifiers work for all players

--- Scenes/battle/battle.gd
+++ Scenes/battle/battle.gd
@@ -20,7 +20,7 @@ func _ready() -> void:
 	for i in run_startup.character_list.size():
 		player_list[i].stats = run_startup.character_list[i]
 
-# Safely disconnect the signal first to avoid any existing connection errors
+	# Safely disconnect the signal first to avoid any existing connection errors
 	if enemy_handler.is_connected(
 		"child_order_changed", Callable(self, "_on_enemies_child_order_changed")
 	):
@@ -28,9 +28,12 @@ func _ready() -> void:
 			"child_order_changed", Callable(self, "_on_enemies_child_order_changed")
 		)
 
-# Now connect the signal again
+	# Now connect the signal again
 	enemy_handler.connect("child_order_changed", Callable(self, "_on_enemies_child_order_changed"))
 
+	battle_ui.player_list = player_list
+
+	enemy_handler.child_order_changed.connect(_on_enemies_child_order_changed)
 	Events.enemy_turn_ended.connect(_on_enemy_turn_ended)
 	Events.player_turn_ended.connect(player_handler.end_turn)
 	Events.player_hand_discarded.connect(enemy_handler.start_turn)
