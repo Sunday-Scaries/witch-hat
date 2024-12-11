01fcb6411b0f78ce73bb239b7d49e8d48b1af837
brianedelman <bd.edelman@gmail.com>
2024-12-10 21:12:14 -0500

    cleanup after rebase

--- Scenes/battle/battle.gd
+++ Scenes/battle/battle.gd
@@ -33,7 +33,6 @@ func _ready() -> void:
 
 	battle_ui.player_list = player_list
 
-	enemy_handler.child_order_changed.connect(_on_enemies_child_order_changed)
 	Events.enemy_turn_ended.connect(_on_enemy_turn_ended)
 	Events.player_turn_ended.connect(player_handler.end_turn)
 	Events.player_hand_discarded.connect(enemy_handler.start_turn)
